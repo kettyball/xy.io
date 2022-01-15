d54d471c1d75ebd91878079c2d5aa4b16aec95da
ketty_ball <932769056@qq.com>
2022-01-15 16:31:59 +0800

    2022/1/15

---
+++ js/login_check.js
@@ -0,0 +1,45 @@
+function testClk(){
+	var obj = document.getElementById("org"); 
+	var sel = document.getElementById("identity");
+}
+
+function validateForm(){
+	var x=document.forms["myForm"]["name"].value;
+    if (x==null || x==""){
+    alert("输入不得为空");
+    return false;
+	}
+}
+
+function setCookie(cname,cvalue,exdays){
+	var d = new Date();
+	d.setTime(d.getTime()+(exdays*24*60*60*1000));
+	var expires = "expires="+d.toGMTString();
+	document.cookie = cname+"="+cvalue+"; "+expires;
+}
+	
+function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++){
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) { 
+			return c.substring(name.length,c.length);
+		}
+	}
+	return "";
+}
+	
+function checkCookie(cname){
+	setCookie("username",cname,30);
+	var user=getCookie("username");
+	if (user!=""){
+		alert("欢迎 " + user + "登录");
+	}
+	else{
+		user = prompt("请输入你的用户名:","");
+  		if (user!="" && user!=null){
+    		setCookie("username",user,30);
+    	}
+	}
+}// JavaScript Document
